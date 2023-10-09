e9d15bc21d0a9ce3212e9d52fb7691fe973cca24
alexvea <35368807+alexvea@users.noreply.github.com>
2023-10-09 10:27:28 +0200

    Update and rename show_install_cmd.sh to show_install_cmd

--- show_install_cmd.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo "cp -f centreon_check_command.class.php /usr/share/centreon/www/api/class/centreon_check_command.class.php"
-echo "cp -f formService.ihtml /usr/share/centreon/www/include/configuration/configObject/service/formService.ihtml"
